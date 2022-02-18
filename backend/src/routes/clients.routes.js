ba634a8a86487b9a0eb19263d0196121e6885e93
Julia Gutierrez <valita129@gmail.com>
2022-02-17 22:03:28 -0400

    Add validations

--- backend/src/routes/clients.routes.js
+++ backend/src/routes/clients.routes.js
@@ -14,7 +14,7 @@ module.exports = function(app, db) {
     clientsController = clientsController(clientsService);
 
     app.get("/api/clients", validations.validateGetById, clientsController.getAllClients);
-    app.post("/api/clients",  clientsController.createClient);
+    app.post("/api/clients", validations.validateCreateClient, clientsController.createClient);
     app.get("/api/clients/:id",  clientsController.findClientById);
     app.delete("/api/clients/:id", clientsController.deleteClientById);
     app.patch("/api/clients/:id", clientsController.updateClient);
--- backend/src/validations/client.validation.js
+++ backend/src/validations/client.validation.js
@@ -3,21 +3,31 @@ const validateTypes = require('./helpers');
 module.exports = function () {
     return {
         validateCreateClient: (req, res, next) => {
-            const schema = Joi.object().keys({
-                client: {
-                    name: Joi.required()
+            const schema = Joi.object({
+                client: Joi.required()
+                .messages({
+                'any.required': `Client is a required field.`
+                })
+            }).keys({
+                client:{
+                    name: Joi.string()
+                    .required()
+                    .min(2)
                     .messages({
-                    'any.required': `Name is a required field.`
+                        'string.base': `"Name" should be a type of 'text'`,
+                        'string.empty': `"Name" cannot be an empty field`,
+                        'string.min': `"Name" should have a minimum length of {#limit}`,
+                        'any.required': `"Name" is a required field.`
                     }),
                     ci: Joi.required()
                     .messages({
                     'any.required': `Ci is a required field.`
-                    })
-                }
-            });
-            return validateTypes.validateRequestBody(req, res, next, schema);
-        },
-        
+                })
+            }
+        });
+        return validateTypes.validateRequestBody(req, res, next, schema);
+    },
+
         validateGetById: (req, res, next) => {
             const schema = Joi.object({
                 size: Joi.required()
